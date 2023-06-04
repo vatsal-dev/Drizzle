f0d5bb261ee0cf89f28681facad7556b891e625f
NandiniGera <nandini.gera18@gmail.com>
2023-06-04 17:10:26 +0530

    final main.py updated

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import argparse
 import json
 import os
 from weather import get_weather
+from weather import extract_weather_data
 from compare_weather import compare_weather
+from compare_weather import get_weather_data
 
 CONFIG_FILE = "config.json"
 flag=0
-
 def get_user_info():
     if os.path.isfile(CONFIG_FILE):
         try:
@@ -21,55 +22,38 @@ def get_user_info():
     else:
         name = input("Enter your name: ")
         default_location = input("Enter your default location: ")
-        compare_weather = input("Do you want to compare weather for two cities? (y/n): ")
-
-        if compare_weather.lower() == 'n':
-            print("Okay! Here's the weather of your location")
-        elif compare_weather.lower() == 'y':
-            flag=1
-            city1 = input("Enter the first city name: ")
-            city2 = input("Enter the second city name: ")
-
-            weather_data_city1 = get_weather(city1)
-            weather_data_city2 = get_weather(city2)
-            compare_weather(city1, weather_data_city1, city2, weather_data_city2)
-        
-
 
     print(f"Hey {name}. Welcome back! Choose an option: \n")
-    compare_weather = input("Do you want to compare weather for two cities? (y/n): ")
+    print("1. Use default location")
+    print("2. Use new location")
+    print("3. Compare Weather of two locations")
+    choice = input("Enter your choice (1 or 2 or 3): ")
 
-    if compare_weather.lower() == 'n':
-        print("Okay! Make your choice then")
-        print("1. Use default location")
-        print("2. Use new location")
-        choice = input("Enter your choice (1 or 2): ")
-
-        if choice == "2":
-             location = input("Enter new location: ")
-        else:
-            location = default_location
-
-        with open(CONFIG_FILE, "w") as file:
-                config_data = {"name": name, "location": location}
-                json.dump(config_data, file)
-
-        return name, location
-
-    elif compare_weather.lower() == 'y':
+    if choice == "2":
+        location = input("Enter new location: ")
+        get_weather(location)
+        
+    elif choice == "3":
         flag=1
         city1 = input("Enter the first city name: ")
         city2 = input("Enter the second city name: ")
 
-        weather_data_city1 = get_weather(city1)
-        weather_data_city2 = get_weather(city2)
+        weather_data_city1 = get_weather_data(city1)
+        weather_data_city2 = get_weather_data(city2)
         compare_weather(city1, weather_data_city1, city2, weather_data_city2)
-    
-
-def main():
-    name, location = get_user_info()
-    if(flag!=1):
+        
+    elif choice == "1":
+        location = default_location
         get_weather(location)
 
+    with open(CONFIG_FILE, "w") as file:
+        config_data = {"name": name, "location": location}
+        json.dump(config_data, file)
+
+    return
+
+def main():
+    get_user_info()
+    
 if __name__ == "__main__":
     main()
