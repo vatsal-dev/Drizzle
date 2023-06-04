e07c5fa5ce139c0d891b0eb1ab65e35a7af6d784
Vatsal <chawlavatsal27@gmail.com>
2023-06-04 12:33:46 +0530

    Enhanced readability of the temperature_trend graph

--- temperature_trend.py
+++ temperature_trend.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_temperature_trend(weather_data):
     dates_list = [forecast["date"] for forecast in weather_data]
     temperature_list = [forecast["temperature"] - 273.15 for forecast in weather_data]  # Convert from Kelvin to Celsius
 
-    plt.plot(dates_list, temperature_list)
+    # Create a higher density x-axis for the smooth curve
+    x = np.linspace(0, len(dates_list) - 1, 2000)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, np.interp(x, range(len(dates_list)), temperature_list), color='blue', linestyle='-', linewidth=2)
+    plt.scatter(range(len(dates_list)), temperature_list, color='blue', marker='o')
     plt.xlabel("Date")
-    plt.ylabel("Temperature (°C)")  # Update the y-axis label
+    plt.ylabel("Temperature (°C)")
     plt.title("Temperature Trend")
-    plt.xticks(rotation=90)
+
+    tick_positions = range(0, len(dates_list), 2)
+    tick_labels = dates_list[::2]
+    plt.xticks(tick_positions, tick_labels, rotation=45, fontsize=8)
+
+    plt.grid(True, linestyle='--', alpha=0.5)
     plt.tight_layout()
     plt.show()
